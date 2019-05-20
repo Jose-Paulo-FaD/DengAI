ff77e0c53e5bf32c7f9f24b5c2e500a51f753e75
José Paulo <>
2019-05-19 23:18:52 -0300

    Implemented Linear Regression

--- dengai.py
+++ dengai.py
@@ -1,26 +1,64 @@
-import utils
-import matplotlib.pyplot as plt
+#import utils
+import preprocessing
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
 
-training_features_url = 'https://raw.githubusercontent.com/Jose-Paulo-FaD/DengAI/master/dengue_features_train.csv'
-training_labels_url = 'https://raw.githubusercontent.com/Jose-Paulo-FaD/DengAI/master/dengue_labels_train.csv'
+training_features_path = './data/dengue_features_train.csv'
+training_labels_path = './data/dengue_labels_train.csv'
 
 # Import data
-feature_frame = pd.read_csv(training_features_url)
-labels_frame = pd.read_csv(training_labels_url)
+feature_frame = pd.read_csv(training_features_path)
+labels_frame = pd.read_csv(training_labels_path)
 
 # Drop redundant columns
 labels_frame.drop(['city', 'year', 'weekofyear'], axis = 1, inplace = True)
 
 # Join features and labels
-all_frame = feature_frame.join(labels_frame)
+all_frame = feature_frame.join(labels_frame).drop(['week_start_date'], axis = 1)
 
 # Separate San Juan and Iquitos
 sj_frame = all_frame.loc[all_frame['city'] == 'sj'].reset_index(drop = True)
 iq_frame = all_frame.loc[all_frame['city'] == 'iq'].reset_index(drop = True)
 
 # Plot correlation matrix, sorted by total cases correlation
-plt.figure(figsize = (8, 8))
-utils.corrplot(all_frame.corr().sort_values('total_cases', axis = 1), size_scale = 150)
+#plt.figure(figsize = (8, 8))
+#utils.corrplot(all_frame.corr().sort_values('total_cases', axis = 1), size_scale = 150)
+
+ratio = 0.75
+n = sj_frame.shape[0]
+
+y_train = sj_frame.iloc[:int(ratio*n), -1].to_frame()
+X_train = sj_frame.iloc[:int(ratio*n), 1:-1]
+
+y_test = sj_frame.iloc[int(ratio*n):, -1].to_frame()
+X_test = sj_frame.iloc[int(ratio*n):, 1:-1]
+
+method = 'mean'
+normalize = True
+
+y_train_p, _ = preprocessing.preprocess(y_train, y_train, method = method, normalize = normalize)
+X_train_p, _ = preprocessing.preprocess(X_train, X_train, method = method, normalize = normalize)
+
+y_test_p, _ = preprocessing.preprocess(y_test, y_train, method = method, normalize = normalize)
+X_test_p, _ = preprocessing.preprocess(X_test, X_train, method = method, normalize = normalize)
+
+model = linear_model.LinearRegression()
+model.fit(X_train_p, y_train_p)
+
+y_hat = model.predict(X_test_p)
+
+plt.subplot(1, 2, 1)
+plt.plot(np.array(y_train_p), 'ob', label = 'actual')
+
+plt.subplot(1, 2, 2)
+
+plt.plot(np.array(y_test_p), 'ob', label = 'actual')
+plt.plot(y_hat, '^r', label = 'predicted')
+
+plt.title(r'R$^2:$ {} | Method: {} | Normalize: {}'.format(str(model.score(X_test_p, y_test_p)), str(method), str(normalize)))
+
+plt.legend(loc='upper right')
 
 plt.show()
